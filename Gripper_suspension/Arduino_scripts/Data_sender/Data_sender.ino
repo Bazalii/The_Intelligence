1223aa71b7b7f2648397c2f1f4d3dfb71c3f38de
Arseniy Alexeev <ars@alexeev.info>
2020-02-23 19:12:47 +0300

    Rewrite Data_sender for time interrupt on atmega2560.

--- Gripper_suspension/Arduino_scripts/Data_sender/Data_sender.ino
+++ Gripper_suspension/Arduino_scripts/Data_sender/Data_sender.ino
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include "HX711.h"
+
 
 // Tensor (+X)
 #define plus_x_DOT 9
@@ -24,17 +27,28 @@
 // Start options
 #define convert_un_to_kg_val 0.035274
 
-HX711 plus_x_dat;
-HX711 minus_x_dat;
-HX711 plus_y_dat;
-HX711 minus_y_dat;
+volatile HX711 plus_x_dat;
+volatile HX711 minus_x_dat;
+volatile HX711 plus_y_dat;
+volatile HX711 minus_y_dat;
 
-float plus_x_units;
-float minus_x_units;
-float plus_y_units;
-float minus_y_units;
+volatile float plus_x_units;
+volatile float minus_x_units;
+volatile float plus_y_units;
+volatile float minus_y_units;
 
 String data_input;
+
+ISR(TIMER5_COMPA_vect)
+{
+plus_x_units = plus_x_dat.get_units(1) * convert_un_to_kg_val;
+minus_x_units = minus_x_dat.get_units(1) * convert_un_to_kg_val;
+plus_y_units = plus_y_dat.get_units(1) * convert_un_to_kg_val;
+minus_y_units = minus_y_dat.get_units(1) * convert_un_to_kg_val;
+
+Serial.println("$ +X" + String(plus_x_units) + " -X" + String(minus_x_units)
+ + " +Y" + String(plus_y_units) + " -Y" + String(minus_y_units) + ";");
+}
 
 void setup(){
 Serial.begin(115200);
@@ -62,26 +76,30 @@ minus_y_dat.begin(minus_y_DOT, minus_y_SCK);
 minus_y_dat.set_scale();
 minus_y_dat.tare(3);
 minus_y_dat.set_scale(calibration_minus_y);
+// инициализация Timer5
+    cli();  // отключить глобальные прерывания
+    TCCR5A = 0;   // установить регистры в 0
+    TCCR5B = 0;
+
+    OCR1A = 15624; // установка регистра совпадения
+
+    TCCR5B |= (1 << WGM12);  // включить CTC режим 
+    TCCR5B |= (1 << CS12); // Установить биты на коэффициент деления 255
+
+    TIMSK5 |= (1 << OCIE5A);  // включить прерывание по совпадению таймера 
+    sei(); // включить глобальные прерывания
 }
 
 void loop(){
-plus_x_units = plus_x_dat.get_units(1) * convert_un_to_kg_val;
-minus_x_units = minus_x_dat.get_units(1) * convert_un_to_kg_val;
-plus_y_units = plus_y_dat.get_units(1) * convert_un_to_kg_val;
-minus_y_units = minus_y_dat.get_units(1) * convert_un_to_kg_val;
+
 
 if (Serial.available()){
     data_input = Serial.readString();
-    data_input.replace(" ", "");
     if (data_input == "tare"){
         minus_x_dat.tare();
         minus_y_dat.tare();
         plus_x_dat.tare();
         plus_y_dat.tare();
     }
-}
-
-Serial.println("$ +X" + String(plus_x_units) + " -X" + String(minus_x_units)
- + " +Y" + String(plus_y_units) + " -Y" + String(minus_y_units) + ";");
-
+  }
 }
