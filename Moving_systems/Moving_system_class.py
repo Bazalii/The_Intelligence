4ca54acd81438322de0fb800e2b0b8ee30f13677
Arseniy Alexeev <ars@alexeev.info>
2020-03-02 23:10:41 +0300

    Add commentaries to MovingSystem abstract class.

--- Moving_systems/Moving_system_class.py
+++ Moving_systems/Moving_system_class.py
@@ -3,19 +3,46 @@ from Classes.Point_class import Point
 
 
 class MovingSystem(ABC):
+    """
+    Абстракционный класс для различный систем перемещения.
+    """
     @abstractmethod
     def move_to_point(self, x: float or Point = None, y: float = None, z: float = None,
                       tx: float = None, ty: float = None, tz: float = None):
+        """
+        Метод перемешения "головы" в звданную точку.
+        :param x: Координата по X
+        :param y: Координата по Y
+        :param z: Координата по Z
+        :param tx: Угол отклонения от оси OX
+        :param ty: Угол отклонения от оси OY
+        :param tz: Угол отклонения от оси OZ
+        :return:
+        """
         pass
 
     @abstractmethod
     def set_zero(self):
+        """
+        Аппаратная установка точки начала отсчета
+        """
         pass
 
     @abstractmethod
     def set_program_zero(self):
+        """
+        Программная установка положения точки начала отсчета
+        """
         pass
 
     @abstractmethod
     def set_speed(self, **kwargs):
+        """
+        Установка скоростей перемещения по осям.
+        :param kwargs:
+        :key x_speed - новая скорость перемещения по OX
+        :key y_speed - новая скорость перемещения по OY
+        :key z_speed - новая скорость перемещения по OZ
+        :return:
+        """
         pass
