730546286c6844b04d6069513b6775abd00158c7
Ivan_Rash <bazalijivan2002@yandex.ru>
2020-03-04 14:14:13 +0300

    Added references 4 Marker_class

--- Classes/Marker_class.py
+++ Classes/Marker_class.py
@@ -1,8 +1,8 @@
 #Класс для маркеров
+from Classes.Point_class import Point
 class Marker():
-    def __init__(self, x, y, origin_x, origin_y):
-        self.x = x
-        self.y = y
+    def __init__(self, center_point:Point, origin_x: int, origin_y: int):
+        self.center_point = center_point
         self.origin_x = origin_x
         self.origin_y = origin_y
 
