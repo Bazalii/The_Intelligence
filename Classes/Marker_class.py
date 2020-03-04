0a9bb23d3553335c7d731d7b404f2dc36561af37
Ivan_Rash <bazalijivan2002@yandex.ru>
2020-03-04 14:17:12 +0300

    Arseniy poshel Naxui

--- Classes/Marker_class.py
+++ Classes/Marker_class.py
@@ -1,7 +1,9 @@
-#Класс для маркеров
+# Класс для маркеров
 from Classes.Point_class import Point
-class Marker():
-    def __init__(self, center_point:Point, origin_x: int, origin_y: int):
+
+
+class Marker:
+    def __init__(self, center_point: Point, origin_x: int, origin_y: int):
         self.center_point = center_point
         self.origin_x = origin_x
         self.origin_y = origin_y
--- Classes/Object_class.py
+++ Classes/Object_class.py
@@ -1,6 +1,8 @@
-#Класс для объектов, находящихся на столе
+# Класс для объектов, находящихся на столе
 from Classes.Marker_class import Marker
-class Object():
+
+
+class Object:
     def __init__(self, marker_1: Marker, marker_2: Marker, center_point: int):
         self.center_point = center_point
         self.marker_1 = marker_1
