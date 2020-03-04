b6f71fac70b466f80dbdbb465419efa3d4eedf06
Ivan_Rash <bazalijivan2002@yandex.ru>
2020-03-04 14:10:12 +0300

    Added references 4 Marker_class

---
+++ Classes/Marker_class.py
@@ -0,0 +1,8 @@
+#Класс для маркеров
+class Marker():
+    def __init__(self, x, y, origin_x, origin_y):
+        self.x = x
+        self.y = y
+        self.origin_x = origin_x
+        self.origin_y = origin_y
+
