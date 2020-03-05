09b1feea3701bcdbf9a246e4457b35c4fc666f75
Arseniy Alexeev <ars@alexeev.info>
2020-03-06 00:26:37 +0300

    Add id and name parameter to Marker class.

--- Classes/Marker_class.py
+++ Classes/Marker_class.py
@@ -3,8 +3,10 @@ from Classes.Point_class import Point
 
 
 class Marker:
-    def __init__(self, center_point: Point, origin_x: int, origin_y: int):
+    def __init__(self, id: int, center_point: Point, origin_x: int, origin_y: int, **kwargs):
         self.center_point = center_point
         self.origin_x = origin_x
         self.origin_y = origin_y
-
+        self.id = id
+        if kwargs.get("name", False):
+            self.name = kwargs["name"]
