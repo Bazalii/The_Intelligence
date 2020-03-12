79817055d054992e1f8fde5a862527b69fadb8c2
Arseniy Alexeev <ars@alexeev.info>
2020-03-12 18:17:26 +0300

    Add aruco detection(Main_program.py)

---
+++ Moving_systems/Parser.py
@@ -0,0 +1,61 @@
+from Classes.Point_class import Point
+# Markers
+Marker_Center = Point(None, None)
+Radius_Vector_of_Marker
+Size_of_Marker
+# OBJECTS
+#         Platform
+Platform_Marker_1_id
+Platform_Marker_2_id
+Platform_Center
+Platform_Length_between_Markers
+Platform_Height_of_Capture
+Platform_Deflection_Angle
+# Motor_1
+Motor_1_Marker_1_id
+Motor_1_Marker_2_id
+Motor_1_Center
+Motor_1_Length_between_Markers
+Motor_1_Height_of_Capture
+Motor_1_Deflection_Angle
+# Motor_2
+Motor_2_Marker_1_id
+Motor_2_Marker_2_id
+Motor_2_Center
+Motor_2_Length_between_Markers
+Motor_2_Height_of_Capture
+Motor_2_Deflection_Angle
+# Motor_3
+Motor_3_Marker_1_id
+Motor_3_Marker_2_id
+Motor_3_Center
+Motor_3_Length_between_Markers
+Motor_3_Height_of_Capture
+Motor_3_Deflection_Angle
+# Motor_4
+Motor_4_Marker_1_id
+Motor_4_Marker_2_id
+Motor_4_Center
+Motor_4_Length_between_Markers
+Motor_4_Height_of_Capture
+Motor_4_Deflection_Angle
+# Electronic_module
+Electronic_module_Marker_1_id
+Electronic_module_Marker_2_id
+Electronic_module_Center
+Electronic_module_Length_between_Markers
+Electronic_module_Height_of_Capture
+Electronic_module_Deflection_Angle
+# Top_part
+Top_part_Marker_1_id
+Top_part_Marker_2_id
+Top_part_Center
+Top_part_Length_between_Markers
+Top_part_Height_of_Capture
+Top_part_Deflection_Angle
+# Table
+Table_Size
+Table_Start_Point
+# Head
+Head_Camera_Legnth
+Head_Length_from_Start_Point_to_Capture_point
