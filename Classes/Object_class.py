bac8fbf6658cd9c01640358cdbb21b38bce45e38
Arseniy Alexeev <ars@alexeev.info>
2020-03-12 19:23:00 +0300

    Add height in Object class.

--- Classes/Object_class.py
+++ Classes/Object_class.py
@@ -3,7 +3,10 @@ from Classes.Marker_class import Marker
 
 
 class Object:
-    def __init__(self, marker_1: Marker, marker_2: Marker, center_point: int):
+    def __init__(self, marker_1: Marker, marker_2: Marker, height, center_point: int, **kwargs):
         self.center_point = center_point
         self.marker_1 = marker_1
         self.marker_2 = marker_2
+        self.height = height
+        if kwargs.get("name", False):
+            self.name = kwargs["name"]
