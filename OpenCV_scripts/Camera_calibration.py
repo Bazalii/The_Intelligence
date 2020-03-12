e9331d8721c097ae503fda335d27968052607347
Arseniy Alexeev <ars@alexeev.info>
2020-03-12 18:15:09 +0300

    Fix bug in Camera_calibration.py.

--- OpenCV_scripts/Camera_calibration.py
+++ OpenCV_scripts/Camera_calibration.py
@@ -9,7 +9,7 @@ def Camera_calibration(**kwargs):
     :key show принимает True или False
     :return: ret, mtx, dist, rvecs, tvecs калибровочные параметры.
     '''
-
+    ret, mtx, dist, rvecs, tvecs = None, None, None, None, None
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -21,13 +21,13 @@ def Camera_calibration(**kwargs):
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
 
-    images = os.listdir("Calibration_images")
+    images = os.listdir("OpenCV_scripts/Calibration_images")
 
     if kwargs.get("show", False):
         cv2.namedWindow("Capture")
 
     for fname in images:
-        img = cv2.imread("Calibration_images/" + fname)
+        img = cv2.imread("OpenCV_scripts/Calibration_images/" + fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
@@ -45,5 +45,7 @@ def Camera_calibration(**kwargs):
             cv2.imshow('Capture', img)
             cv2.waitKey(1000)
             ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    if kwargs.get("show", False):
+        cv2.destroyAllWindows()
 
-    cv2.destroyAllWindows()
+    return ret, mtx, dist, rvecs, tvecs
